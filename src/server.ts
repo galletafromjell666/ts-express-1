73c32c4c5d801bbf73549c5fabe52ea329dd6ce4
galletafromjell666 <giococu666@gmail.com>
2023-01-27 20:20:29 -0600

    create folders

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+
+const router = express();
+
+// connect to mongo
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        console.log('connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
